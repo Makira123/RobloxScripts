4fe77f4a1dc903dd61412f4e456d8b3331c72a28
Makira123 <167422086+Makira123@users.noreply.github.com>
2026-04-22 03:49:30 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,7 @@ client.on("messageCreate", (message) => {
       }
     });
   }
+  console.log("bot",message.id)
   axios.post("https://discord-mheg.onrender.com/api/message", {
     user: msgData.user,
     text: msgData.text,
@@ -131,6 +132,7 @@ client.on("ready", async () => {
             msgData.videos.push(att.url);
           }
         });
+        console.log("bots",message.id
 
         axios.post("https://discord-mheg.onrender.com/api/message", {
           user: msgData.user,
